0984b2c175d685ccc9c99389b2c1f166a283219c
Radmila <rmarinkovic@gmail.com>
2014-06-07 23:59:18 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,7 +3,8 @@ data<-read.table("household_power_consumption.txt", sep=";",header=TRUE, na.stri
 data1<-subset(data, Date=="1/2/2007" | Date=="2/2/2007") ##subset to only see two dates
 data2<-as.Date(data1$Date, format="%d/%m/%Y") ## formatting dates
 datumfix<-paste(data2, data1$Time)
-timefix <- strptime(datumfix, format="%Y-%m-%d %H:%M:%S")## str(dates)
+timefix <- strptime(datumfix, format="%Y-%m-%d %H:%M:%S")## formatting date and time for the plot
+##Plot the requested data
 plot(timefix,data1$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="",pch=33)
 lines(timefix,data1$Global_active_power)
 dev.off()
