07d59c5a497603d21149fc2dc302bc55695e30df
Radmila <rmarinkovic@gmail.com>
2014-06-08 00:01:10 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,7 +3,8 @@ data<-read.table("household_power_consumption.txt", sep=";",header=TRUE, na.stri
 data1<-subset(data, Date=="1/2/2007" | Date=="2/2/2007") ##subset to only see two dates
 data2<-as.Date(data1$Date, format="%d/%m/%Y") ## formatting dates
 datumfix<-paste(data2, data1$Time)
-timefix <- strptime(datumfix, format="%Y-%m-%d %H:%M:%S")## str(dates)
+timefix <- strptime(datumfix, format="%Y-%m-%d %H:%M:%S")
+## Plot sub_metering variables and add legend
 plot(timefix,data1$Sub_metering_1, ylab="Energy sub metering", xlab="",type="l", pch=33, col="black")
 lines(timefix,data1$Sub_metering_2, type="l", col="red")
 lines(timefix,data1$Sub_metering_3, type="l", col="blue")
