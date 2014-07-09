ee5907c9a3f2440f6b13d05dbbfd250232560ac3
Radmila <rmarinkovic@gmail.com>
2014-07-09 09:27:38 -0400

    Update plot1.R
    Edited final

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 png("plot1.png", 480, 480)
 data<-read.table("household_power_consumption.txt", sep=";",header=TRUE, na.strings="?")
-data1<-subset(data, Date=="1/2/2007" | Date=="2/2/2007") ##subset to only see two dates
+data1<-subset(data, Date=="1/2/2007" | Date=="2/2/2007") ##subset data to only see two dates
 histogram<-hist(data1$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
 dev.off()
